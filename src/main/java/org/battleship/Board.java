def138d2aa785616686de1e130a603bdd6414ad0
nswieconek <norbert.swieconek@spartavity.com>
2025-11-13 18:47:53 +0100

    feat: Initialize board grid in constructor

--- src/main/java/org/battleship/Board.java
+++ src/main/java/org/battleship/Board.java
@@ -1,9 +1,15 @@
 package org.battleship;
 
+import java.util.Arrays;
+
 public class Board {
     private char[][] grid;
 
     public Board () {
+        this.grid = new char[10][10];
 
+        for (char[] chars : grid) {
+            Arrays.fill(chars, '~');
+        }
     }
 }
