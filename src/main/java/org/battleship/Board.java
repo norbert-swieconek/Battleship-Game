a794a5148569764684c926227834d5de59365d5b
nswieconek <norbert.swieconek@spartavity.com>
2025-11-14 20:56:21 +0100

    feat: Implement displayBoard method and render empty board in Main

--- src/main/java/org/battleship/Board.java
+++ src/main/java/org/battleship/Board.java
@@ -5,11 +5,32 @@ import java.util.Arrays;
 public class Board {
     private char[][] grid;
 
+    // filling grid with '~' chars
     public Board () {
         this.grid = new char[10][10];
-
         for (char[] chars : grid) {
             Arrays.fill(chars, '~');
+        }
+    }
+
+    // displaying full board on console
+    public void displayBoard() {
+        // printing number of column from 1 to 10
+        System.out.print("  ");
+        for (int i = 1; i < 11; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // printing name of row from A to J and battlefield
+        char letter = 'A';
+        for (char[] row : grid) {
+            System.out.print(letter + " ");
+            letter++;
+            for (char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
         }
     }
 }
--- src/main/java/org/battleship/Main.java
+++ src/main/java/org/battleship/Main.java
@@ -2,6 +2,7 @@ package org.battleship;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Board board = new Board();
+        board.displayBoard();
     }
 }
